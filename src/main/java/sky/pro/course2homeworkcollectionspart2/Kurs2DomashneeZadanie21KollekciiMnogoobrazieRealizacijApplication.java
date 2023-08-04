926f527cf1f4cb81d05ddb17528b6892bd3073f0
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-04 10:31:04 +0100

    2 Курс ДЗ №21 Коллекции многообразие реализаций

---
+++ src/main/java/sky/pro/course2homeworkcollectionspart2/Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homeworkcollectionspart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2homeworkcollectionspart2/Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homeworkcollectionspart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomashneeZadanie21KollekciiMnogoobrazieRealizacijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
